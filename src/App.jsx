9f9c901ebd7172c254022007e30241d8c9b4c93c
lenlo121500 <raulc8808@gmail.com>
2025-06-23 17:16:53 +0800

    added download section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
 import FAQ from "./sections/FAQ";
 import Testimonials from "./sections/Testimonials";
+import Download from "./sections/Download";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Pricing />
       <FAQ />
       <Testimonials />
+      <Download />
     </main>
   );
 };
