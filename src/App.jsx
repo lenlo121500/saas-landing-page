ca75db04040fd9a59fea1c9a38fabb1b7ce60910
lenlo121500 <raulc8808@gmail.com>
2025-06-23 16:42:18 +0800

    added testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
 import FAQ from "./sections/FAQ";
+import Testimonials from "./sections/Testimonials";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Features />
       <Pricing />
       <FAQ />
+      <Testimonials />
     </main>
   );
 };
