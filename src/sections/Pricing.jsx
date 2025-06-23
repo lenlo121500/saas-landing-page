4f87809624b5923f282a914a207d23867024a2d2
lenlo121500 <raulc8808@gmail.com>
2025-06-23 18:06:29 +0800

    remove whitespaces

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -103,7 +103,7 @@ const Pricing = () => {
                         index === 1 ? "text-p3" : "text-p4"
                       )}
                     >
-                      ${" "}
+                      $
                       <CountUp
                         start={plan.priceMonthly}
                         end={monthly ? plan.priceMonthly : plan.priceYearly}
