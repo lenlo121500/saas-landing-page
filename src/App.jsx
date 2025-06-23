93f03a91061b7708c8f054e3d7fe909a7366350b
lenlo121500 <raulc8808@gmail.com>
2025-06-23 16:19:15 +0800

    added FAQ section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
+import FAQ from "./sections/FAQ";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <Features />
       <Pricing />
+      <FAQ />
     </main>
   );
 };
